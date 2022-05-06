a86cb16c2340de56b8f7e91655753704b69d6f93
Javivzk <93289840+Javivzk@users.noreply.github.com>
2022-05-06 19:14:57 +0200

    Añado la clase Jugador

---
+++ src/main/java/com/svalero/examenjavier/Jugador.java
@@ -0,0 +1,28 @@
+package com.svalero.examenjavier;
+
+public class Jugador {
+
+    private String nombre;
+    private Carta misCartas[];
+    private int puntos;
+
+    public Jugador(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void preguntarMovimiento(){
+
+    }
+
+    public void addCarta(Carta carta){
+
+    }
+
+    public void imprimirCartas(){
+
+    }
+
+    public void resetearRonda(){
+
+    }
+}
