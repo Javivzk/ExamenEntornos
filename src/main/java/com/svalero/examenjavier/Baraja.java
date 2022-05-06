7383b7aba7d667b0b925d1d9860bdf4dab736b4e
Javivzk <93289840+Javivzk@users.noreply.github.com>
2022-05-06 19:09:46 +0200

    Añado la clase baraja

---
+++ src/main/java/com/svalero/examenjavier/Baraja.java
@@ -0,0 +1,27 @@
+package com.svalero.examenjavier;
+
+
+public class Baraja {
+
+    private Carta listaCartas[];
+
+    private int num_cartas = 52;
+
+    public Baraja(){
+
+    }
+
+    public void barajar(){
+
+    }
+
+    public Carta cogerCarta(){
+
+        return null;
+    }
+
+    public Carta[] repartirMano(){
+
+        return new Carta[0];
+    }
+}
